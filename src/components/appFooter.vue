55e7104ed76adb4ad7e6ca9e44d8e63869659717
gzzhangyingying <gzzhangyingying@corp.netease.com>
2017-08-14 09:49:43 +0800

    components

---
+++ src/components/appFooter.vue
@@ -0,0 +1,37 @@
+<template>
+  <tabbar>
+    <tabbar-item>
+      <!--<img slot="icon" src="../assets/demo/icon_nav_button.png">-->
+      <span slot="label">微信</span>
+    </tabbar-item>
+    <tabbar-item show-dot>
+      <!--<img slot="icon" src="../assets/demo/icon_nav_msg.png">-->
+      <span slot="label">通讯录</span>
+    </tabbar-item>
+    <tabbar-item selected >
+      <!--<img slot="icon" src="../assets/demo/icon_nav_article.png">-->
+      <span slot="label">发现</span>
+    </tabbar-item>
+    <tabbar-item badge="2">
+      <!--<img slot="icon" src="../assets/demo/icon_nav_cell.png">-->
+      <span slot="label">我</span>
+    </tabbar-item>
+  </tabbar>
+</template>
+<script>
+  import { Tabbar, TabbarItem } from 'vux'
+
+  export default {
+    name: 'app-footer',
+    components: {
+      Tabbar,
+      TabbarItem
+    },
+    data () {
+      return {
+//        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
---
+++ src/components/dialogBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="dialog-box">
+    <div></div>
+  </div>
+</template>
+<script>
+  import { Panel } from 'vux '
+  export default {
+    name: 'dialog-box',
+    components: {
+      Panel
+    },
+    data () {
+      return {
+//        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+<style>
+  .dialog-box{
+    border-top:1px solid #eee;
+    border-bottom: 1px solid #eee;
+  }
+</style>
